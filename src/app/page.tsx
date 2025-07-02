b1182f1c23fce191ae9f6aa44c7e6186a8289c8e
wordisstuff <wordisstuff@gmail.com>
2025-07-02 14:05:44 -0500

    start= 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Header from './components/header/header';
+// import Header from './components/header/header';
 
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <header>
+            {/* <header>
                 <Header>Header</Header>
-            </header>
+            </header> */}
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <h1 className="text-green-500">HELLO</h1>
+                <h1 className="text-gray-500">
+                    HELLO {new Date().toTimeString()}
+                </h1>
             </main>
             <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
                 <h2>FOOTER</h2>
