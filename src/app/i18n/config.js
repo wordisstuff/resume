7781a8f61350dac7ea77819ffc4c7b448b327505
wordisstuff <wordisstuff@gmail.com>
2025-07-18 05:52:25 +0000

    i18n

--- src/app/i18n/config.js
+++ src/app/i18n/config.js
@@ -1,12 +1,10 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LangDetector from 'i18next-browser-languagedetector';
 import us from './languages/us.json';
 import ua from './languages/ua.json';
 
 i18next
     .use(initReactI18next)
-    .use(LangDetector)
     .init({
         fallbackLng: 'us',
         debug: false,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { useTranslation } from 'react-i18next';
 import Header from './components/header/header';
+import './i18n/config.js';
 
 // import { headers } from 'next/headers';
 
 export default function Home() {
     const { t } = useTranslation();
+    console.log(t('header.menu'))
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <header>
@@ -13,7 +15,7 @@ export default function Home() {
             </header>
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
                 <h1 className="text-gray-500">
-                    HELLO {new Date().toTimeString()}
+                    HELLO
                 </h1>
             </main>
             <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
